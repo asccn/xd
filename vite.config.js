55e48d47b7f4cb04d8ae961be3f945bd57674d5e
sujan khatri <sujansince2059@gmail.com>
2023-11-06 22:42:54 +0545

    f

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/xd/dist",
 });
 
 // base: "https://sujansince2003.github.io/ngo",
